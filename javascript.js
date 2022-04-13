ebc56a0704f9bcceffc7d5c2d711f8bfb450ae51
Amir2170 <you@example.com>
2022-04-13 16:48:59 +0430

    finished slideshow + footer implementin rating system

--- javascript.js
+++ javascript.js
@@ -37,3 +37,39 @@ document.addEventListener('click', function(event) {
 
   arrow.classList.toggle('rotate');
 });
+
+
+/* Slide Show For Aside Book Reccomendation */
+
+let slideIndex = 1;
+showSlides(slideIndex);
+
+// Next/Prev Control
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName('mySlides');
+  let dots = document.getElementsByClassName('dot');
+  if(n > slides.length) slideIndex = 1;
+  if(n < 1) slideIndex = slides.length;
+
+  for(i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+
+  for(i = 0; i < dots.length; i++) {
+    dots[i].classList.remove('active');
+  }
+
+  dots[slideIndex - 1].classList.add('active');
+  slides[slideIndex - 1].style.display = "block";
+}
+ 
+
